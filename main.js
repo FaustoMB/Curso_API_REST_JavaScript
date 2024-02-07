aba3bc3898ba5b64bb6b70d136bb4f9cf8442fe3
Fausto Mastrodomenico <faustomastro96@gmail.com>
2024-02-07 15:09:41 -0500

    Endpoints y query parameters

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 const button = document.getElementById('reload');
-const URL = "https://api.thecatapi.com/v1/images/search?limit"
+const URL = "https://api.thecatapi.com/v1/images/search?limit=3"
 
 button.addEventListener('click',fetchData)
 
 async function fetchData(){
-    const res = await fetch(URL)
-    const data = await res.json()
-    const img = document.querySelector('img')
-    img.src = data[0].url;
+    const res = await fetch(URL);
+    const data = await res.json();
+
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
 }
 
 fetchData();
